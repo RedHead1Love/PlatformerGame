60854f58cecaa0cda64cf24492e88f685d6e5c80
RedHead1Love <stand_upper@mail.ru>
2026-05-23 00:42:51 +0500

    Update IPersistentWallet.cs

--- Assets/Scripts/Wallet/Interfaces/IPersistentWallet.cs
+++ Assets/Scripts/Wallet/Interfaces/IPersistentWallet.cs
@@ -3,7 +3,7 @@ namespace GameLogic
     public interface IPersistentWallet : IWalletManager
     {
         CoinData CurrentCoins { get; }
-        
+
         void SaveCoins();
         void LoadCoins();
         void LoadFromSaveData(CoinData data);
