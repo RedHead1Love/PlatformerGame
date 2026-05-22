6ab0dd1edab8db2819730dbafea77f2b566158e3
RedHead1Love <stand_upper@mail.ru>
2026-05-22 19:22:03 +0500

    Refactor IWalletManager to WalletManager class

--- Assets/Scripts/Wallet/Interfaces/IWalletManager.cs
+++ Assets/Scripts/Wallet/Interfaces/IWalletManager.cs
@@ -1,20 +1,82 @@
 using System;
-using System.Collections.Generic;
+using UnityEngine;
 
 namespace GameLogic
 {
-    public interface IWalletManager
+    public class WalletManager : MonoBehaviour, IWalletManager
     {
-        event Action<WalletManager.CoinType, int> OnCoinsChanged;
+        public enum CoinType
+        {
+            Bronze,
+            Silver,
+            Gold
+        }
 
-        void AddCoins(WalletManager.CoinType type, int amount);
-        bool TrySpendCoins(WalletManager.CoinType type, int amount);
-        int GetCoins(WalletManager.CoinType type);
+        public static WalletManager Instance { get; protected set; }
 
-        Dictionary<WalletManager.CoinType, int> GetAllCoins();
+        public event Action<CoinType, int> OnCoinsChanged;
 
-        void SaveWallet();
-        void LoadWallet();
-        void LoadFromSaveData(CoinData saveData);
+        protected Wallet _internalWallet;
+
+        protected virtual void Awake()
+        {
+            InitializeSingleton();
+        }
+
+        protected void InitializeSingleton()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                _internalWallet = new Wallet();
+                
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public virtual int GetCoins(CoinType type)
+        {
+            return _internalWallet?.GetCoins(type) ?? 0;
+        }
+
+        public virtual void AddCoins(CoinType type, int amount)
+        {
+            if (_internalWallet == null)
+            {
+                return;
+            }
+
+            _internalWallet.AddCoins(type, amount);
+            NotifyCoinsChanged(type);
+        }
+
+        public virtual bool SpendCoins(CoinType type, int amount)
+        {
+            if (_internalWallet != null && _internalWallet.SpendCoins(type, amount))
+            {
+                NotifyCoinsChanged(type);
+                return true;
+            }
+            
+            return false;
+        }
+
+        public virtual void ResetWallets()
+        {
+            _internalWallet?.Reset();
+            
+            NotifyCoinsChanged(CoinType.Bronze);
+            NotifyCoinsChanged(CoinType.Silver);
+            NotifyCoinsChanged(CoinType.Gold);
+        }
+
+        protected void NotifyCoinsChanged(CoinType type)
+        {
+            OnCoinsChanged?.Invoke(type, GetCoins(type));
+        }
     }
 }
