17d3de4aa4413c5317636afbc80a9c9eecc6823b
RedHead1Love <stand_upper@mail.ru>
2026-05-22 19:24:04 +0500

    Refactor IWalletManager interface and remove implementation

--- Assets/Scripts/Wallet/Interfaces/IWalletManager.cs
+++ Assets/Scripts/Wallet/Interfaces/IWalletManager.cs
@@ -1,82 +1,11 @@
 using System;
-using UnityEngine;
 
 namespace GameLogic
 {
-    public class WalletManager : MonoBehaviour, IWalletManager
+    public interface IWalletManager : IWallet
     {
-        public enum CoinType
-        {
-            Bronze,
-            Silver,
-            Gold
-        }
-
-        public static WalletManager Instance { get; protected set; }
-
-        public event Action<CoinType, int> OnCoinsChanged;
-
-        protected Wallet _internalWallet;
-
-        protected virtual void Awake()
-        {
-            InitializeSingleton();
-        }
-
-        protected void InitializeSingleton()
-        {
-            if (Instance == null)
-            {
-                Instance = this;
-                _internalWallet = new Wallet();
-                
-                DontDestroyOnLoad(gameObject);
-            }
-            else if (Instance != this)
-            {
-                Destroy(gameObject);
-            }
-        }
-
-        public virtual int GetCoins(CoinType type)
-        {
-            return _internalWallet?.GetCoins(type) ?? 0;
-        }
-
-        public virtual void AddCoins(CoinType type, int amount)
-        {
-            if (_internalWallet == null)
-            {
-                return;
-            }
-
-            _internalWallet.AddCoins(type, amount);
-            NotifyCoinsChanged(type);
-        }
-
-        public virtual bool SpendCoins(CoinType type, int amount)
-        {
-            if (_internalWallet != null && _internalWallet.SpendCoins(type, amount))
-            {
-                NotifyCoinsChanged(type);
-                return true;
-            }
-            
-            return false;
-        }
-
-        public virtual void ResetWallets()
-        {
-            _internalWallet?.Reset();
-            
-            NotifyCoinsChanged(CoinType.Bronze);
-            NotifyCoinsChanged(CoinType.Silver);
-            NotifyCoinsChanged(CoinType.Gold);
-        }
-
-        protected void NotifyCoinsChanged(CoinType type)
-        {
-            OnCoinsChanged?.Invoke(type, GetCoins(type));
-        }
+        event Action<WalletManager.CoinType, int> OnCoinsChanged;
+        
+        void ResetWallets();
     }
 }
