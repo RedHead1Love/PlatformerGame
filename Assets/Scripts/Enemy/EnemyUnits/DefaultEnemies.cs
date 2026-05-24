c07ff5086a673a72acc5daf06914d55e2fe525ef
RedHead1Love <stand_upper@mail.ru>
2026-05-24 18:20:10 +0500

    Update DefaultEnemies.cs

--- Assets/Scripts/Enemy/EnemyUnits/DefaultEnemies.cs
+++ Assets/Scripts/Enemy/EnemyUnits/DefaultEnemies.cs
@@ -11,9 +11,7 @@ namespace EnemyLogicDefault
         private const string PlayerLayerName = "Player";
         private const float DestroyDelay = 1f;
         private const float MovementThreshold = 0.01f;
-        private const float DirectionThreshold = 0f;
         private const float BoxRotationAngle = 0f;
-        private const int HealthThreshold = 0;
 
         [Header("Attack Settings")]
         [SerializeField] private float _attackCooldown = 1f;
@@ -23,50 +21,177 @@ namespace EnemyLogicDefault
         [SerializeField] private Vector2 _attackOffset = new Vector2(1.5f, 0.8f);
         [SerializeField] private Vector2 _attackSize = new Vector2(2.5f, 1.6f);
 
-        [Header("Attack Timing")]
-        [SerializeField] private float _attackDuration = 0.5f;
-        [SerializeField] private float _attackPause = 0.4f;
-
         [Header("Hurt Settings")]
-        [SerializeField] private float _hurtInvulnerabilityDuration = 0.1f;
+        [SerializeField] private float _hurtInvulnerabilityDuration = 0.5f;
 
         private Animator _animator;
         private PatrolAI _patrolAI;
-        private EnemyAudioController _audioController;
 
         private bool _isHurt;
         private bool _isAttacking;
         private float _nextAttackTime;
         private bool _isPlayerInRange;
-        private bool _wasPlayerInRangeBeforeHurt;
+        private LayerMask _playerLayerMask;
 
-        public override void Die()
+        private enum AnimationState
         {
-            CancelInvoke(nameof(EndHurtState));
-            CancelInvoke(nameof(OnAttackEnded));
-
-            if (_patrolAI != null)
-            {
-                _patrolAI.enabled = false;
-            }
-
-            enabled = false;
-
-            SetAnimationState(AnimationState.Death);
-            PlayDeathSound();
-
-            base.Die();
-
-            Destroy(gameObject, DestroyDelay);
+            Idle = 0,
+            Walk = 1,
+            Attack = 2,
+            Hurt = 3,
+            Death = 4
         }
 
-        private void OnDestroy()
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _animator = GetComponent<Animator>();
+            _patrolAI = GetComponent<PatrolAI>();
+            _playerLayerMask = LayerMask.GetMask(PlayerLayerName);
+        }
+
+        private void OnEnable()
+        {
+            if (_patrolAI != null)
+            {
+                _patrolAI.OnMoveDirectionChanged += HandleMovementDirectionChanged;
+                _patrolAI.OnInAttackRange += HandleAttackRangeChanged;
+            }
+        }
+
+        private void OnDisable()
         {
             if (_patrolAI != null)
             {
                 _patrolAI.OnMoveDirectionChanged -= HandleMovementDirectionChanged;
                 _patrolAI.OnInAttackRange -= HandleAttackRangeChanged;
             }
+        }
+
+        private void Update()
+        {
+            if (IsDead || _isHurt)
+            {
+                return;
+            }
+
+            CheckAttackCondition();
+        }
+
+        private void HandleMovementDirectionChanged(Vector2 direction)
+        {
+            if (IsDead || _isHurt || _isAttacking)
+            {
+                return;
+            }
+
+            if (direction.magnitude > MovementThreshold)
+            {
+                SetAnimationState(AnimationState.Walk);
+                FlipSprite(direction.x);
+            }
+            else
+            {
+                SetAnimationState(AnimationState.Idle);
+            }
+        }
+
+        private void FlipSprite(float directionX)
+        {
+            if ((directionX > 0 && transform.localScale.x < 0) || (directionX < 0 && transform.localScale.x > 0))
+            {
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
+        }
+
+        private void HandleAttackRangeChanged(bool inRange)
+        {
+            _isPlayerInRange = inRange;
+        }
+
+        private void CheckAttackCondition()
+        {
+            if (_isPlayerInRange && !_isAttacking && Time.time >= _nextAttackTime)
+            {
+                StartAttack();
+            }
+        }
+
+        private void StartAttack()
+        {
+            _isAttacking = true;
+
+            SetAnimationState(AnimationState.Attack);
+        }
+
+        public override void TakeDamage(int amount)
+        {
+            if (IsDead || _isHurt)
+            {
+                return;
+            }
+
+            base.TakeDamage(amount);
+
+            if (!IsDead)
+            {
+                TriggerHurtState();
+            }
+        }
+
+        private void TriggerHurtState()
+        {
+            _isHurt = true;
+            _isAttacking = false;
+
+            SetAnimationState(AnimationState.Hurt);
+
+            Invoke(nameof(EndHurtState), _hurtInvulnerabilityDuration);
+        }
+
+        private void EndHurtState()
+        {
+            if (IsDead)
+            {
+                return;
+            }
+
+            _isHurt = false;
+
+            if (_isPlayerInRange)
+            {
+                SetAnimationState(AnimationState.Idle);
+            }
+            else if (_patrolAI != null && _patrolAI.CurrentMoveDirection.magnitude > MovementThreshold)
+            {
+                SetAnimationState(AnimationState.Walk);
+            }
+            else
+            {
+                SetAnimationState(AnimationState.Idle);
+            }
+        }
+
+        public override void Die()
+        {
+            CancelInvoke(nameof(EndHurtState));
+
+            _isHurt = false;
+            _isAttacking = false;
+
+            if (_patrolAI != null)
+            {
+                _patrolAI.enabled = false;
+            }
+
+            SetAnimationState(AnimationState.Death);
+
+            base.Die();
+
+            Destroy(gameObject, DestroyDelay);
         }
 
         private void SetAnimationState(AnimationState state)
@@ -77,23 +202,31 @@ namespace EnemyLogicDefault
             }
         }
 
-        private void PlayDeathSound()
+        public void AnimEvent_DealDamage()
         {
-            _audioController?.PlayDeathSound();
+            Vector2 boxCenter = (Vector2)transform.position + new Vector2(_attackOffset.x * Mathf.Sign(transform.localScale.x), _attackOffset.y);
+            Collider2D hit = Physics2D.OverlapBox(boxCenter, _attackSize, BoxRotationAngle, _playerLayerMask);
+
+            if (hit != null)
+            {
+                IDamageable damageable = hit.GetComponent<IDamageable>() ?? hit.GetComponentInParent<IDamageable>();
+                damageable?.TakeDamage(_attackDamage);
+            }
         }
 
-        private void EndHurtState() { }
-        private void OnAttackEnded() { }
-        private void HandleMovementDirectionChanged(Vector2 direction) { }
-        private void HandleAttackRangeChanged(bool inRange) { }
-
-        private enum AnimationState
+        public void AnimEvent_EndAttack()
         {
-            Idle,
-            Walk,
-            Attack,
-            Hurt,
-            Death
+            _isAttacking = false;
+            _nextAttackTime = Time.time + _attackCooldown;
+
+            SetAnimationState(_isPlayerInRange ? AnimationState.Idle : AnimationState.Walk);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Vector2 boxCenter = (Vector2)transform.position + new Vector2(_attackOffset.x * Mathf.Sign(transform.localScale.x), _attackOffset.y);
+            Gizmos.DrawWireCube(boxCenter, _attackSize);
         }
     }
 }
